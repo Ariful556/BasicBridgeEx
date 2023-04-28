8fe41a3056f1ab984e064a1cc28c1f017469359e
Md. Ariful Islam <mdariful.islam@bjitacademy.com>
2023-04-28 10:58:59 +0600

    fan change to sout

--- src/ExampleBP/Fan.java
+++ src/ExampleBP/Fan.java
@@ -2,7 +2,7 @@ package ExampleBP;
 
 public class Fan implements ElectricalEquipment {
     public void PowerOn() {
-        System.out.println("Fan is On");
+        System.out.println("Fan is Onn");
     }
 
     public void PowerOff() {
